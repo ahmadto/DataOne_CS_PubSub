93d73f262681ff979abc12bc330b89b5e5eb7144
ahmadto <ahmad.nahhas@dataoneglobal.com>
2016-09-14 10:59:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PubNubSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PubNubSub pSub = new PubNubSub();
+            pSub.initialize();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PubNubSub.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PubNubMessaging.Core;
+
+namespace PubNubSub
+{
+    class PubNubSub
+    {
+        Pubnub pubnub;
+
+        public void initialize()
+        {
+            pubnub = new Pubnub("", "PUT SUB KEY HERE");
+
+            pubnub.Subscribe<string>("PUT CHANNEL HERE", DisplaySubscribeReturnMessage, DisplaySubscribeConnectStatusMessage, DisplayErrorMessage);
+        }
+
+        void DisplaySubscribeReturnMessage(string result)
+        {
+            Console.WriteLine("SUBSCRIBE REGULAR CALLBACK:");
+            Console.WriteLine(result);
+            if (!string.IsNullOrEmpty(result) && !string.IsNullOrEmpty(result.Trim()))
+            {
+                List<object> deserializedMessage = pubnub.JsonPluggableLibrary.DeserializeToListOfObject(result);
+                if (deserializedMessage != null && deserializedMessage.Count > 0)
+                {
+                    object subscribedObject = (object)deserializedMessage[0];
+                    if (subscribedObject != null)
+                    {
+                        //IF CUSTOM OBJECT IS EXCEPTED, YOU CAN CAST THIS OBJECT TO YOUR CUSTOM CLASS TYPE
+                        string resultActualMessage = pubnub.JsonPluggableLibrary.SerializeToJsonString(subscribedObject);
+                    }
+                }
+            }
+        }
+        
+        void DisplaySubscribeConnectStatusMessage(string result)
+        {
+            Console.WriteLine("SUBSCRIBE CONNECT CALLBACK");
+        }
+
+        void DisplayErrorMessage(PubnubClientError pubnubError)
+        {
+            Console.WriteLine(pubnubError.StatusCode);
+        }
+    }
+}
